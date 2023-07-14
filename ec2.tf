96bb090b7002573424e72def3533739a51ebe663
IrynaDorokhina <iryna.dorokhina.28@gmail.compwdgit status>
2023-07-14 16:28:57 +0200

    file issue fixed

--- ec2.tf
+++ ec2.tf
@@ -4,17 +4,18 @@ resource "aws_instance" "wordpress"{
     key_name = "vockey"
     vpc_security_group_ids = [aws_security_group.devVPC_sg_allow_http.id]
     subnet_id = aws_subnet.devVPC_public_subnet_1.id
+    user_data = "${file("template.tpl")}"
     tags = {
         Name = "wordpress"
     }
 
-    data "template_file" "init" {
-    template = "${file("${path.module}/template.tpl")}"
-    vars =  {
-        consul_address = "${aws_instance.consul.private.ip}"
+    #data "template_file" "init" {
+    #template = "${file("${path.module}/template.tpl")}"
+    #vars =  {
+    #    consul_address = "${aws_instance.consul.private.ip}"
+    #    }
+    }
 
-        }
-}
     provisioner "local-exec"{
         command = "echo Instance Type=${self.instance_type},Instance ID=${self.id},Public DNS=${self.public_dns},AMI ID=${self.ami} >> allinstancedetails"
     }
