b7e4bb83e78c74beb351aa53eaaf9901253c0163
IrynaDorokhina <iryna.dorokhina.28@gmail.compwdgit status>
2023-07-19 18:20:42 +0200

    wednesday

--- vpc.tf
+++ vpc.tf
@@ -103,6 +103,16 @@ resource "aws_security_group" "devVPC_sg_allow_ssh_http"{
     }
 }
 
+# Ingress Security Port 22 (Inbound) - Provides a security group rule resource (https://registry.terraform.io.providers/hashicorp/aws/latest/docs/resources/security_group_rule)
+resource "aws_security_group_rule" "devVPC_ssh_ingress_access"{
+    from_port             = 22
+    protocol              = "tcp"
+    security_group_id     = aws_security_group.devVPC_sg_allow_ssh_http.id
+    to_port               = 22
+    type                  = "ingress"
+    cidr_blocks           = [var.cidr_blocks]
+}
+
 # Ingress Security Port 80 (Inbound)
 resource "aws_security_group_rule" "devVPC_http_ingress_access"{
     from_port = 80
