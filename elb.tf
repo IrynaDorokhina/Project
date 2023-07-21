59415be9c9efac8c3278e11c53ba2f76e5862d18
IrynaDorokhina <iryna.dorokhina.28@gmail.compwdgit status>
2023-07-21 11:09:37 +0200

    friday challange elb

--- elb.tf
+++ elb.tf
@@ -1,3 +1,8 @@
+resource "aws_ami_from_instance" "wordpress" {
+  name               = "ami_wordpress"
+  source_instance_id = aws_instance.wordpress.id
+}
+
 resource "aws_lb_target_group" "front" {
     name = "application-front"
     port = 80
@@ -27,7 +32,7 @@ resource "aws_lb" "front" {
     internal = false
     load_balancer_type = "application"
     security_groups = [aws_security_group.devVPC_sg_allow_ssh_http.id]
-    subnets= [aws_subnet.devVPC_public_subnet_1.id] #FIX
+    subnets= [aws_subnet.devVPC_public_subnet_1.id, aws_subnet.devVPC_public_subnet_2.id] #FIX
     enable_deletion_protection = true
     tags = {
     Environment = "front"
